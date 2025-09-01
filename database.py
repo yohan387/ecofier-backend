86193e8a9d16f89669e07adb8088540c5781753a
Bonheur Tam <bonheurnouba@gmail.com>
2025-09-01 21:06:34 +0000

    Merge done

--- database.py
+++ database.py
@@ -1,17 +1,17 @@
 # database.py
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
-from config import DB_NAME, DB_URL
-
-CLIENT_MONGO: AsyncIOMotorClient | None = None
-DATABASE: AsyncIOMotorDatabase | None = None
 
 
-async def init_db():
-    global CLIENT_MONGO, DATABASE
-    CLIENT_MONGO = AsyncIOMotorClient("mongodb://localhost:27017/")
-    DATABASE = CLIENT_MONGO["ecofier"]
-    print("✅ Database initialized")
+DATABASE_URL = "mongodb+srv://admin:admin@cluster0.hagx7ew.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DATABASE_NAME = 'ecofier_1'
 
+CLIENT_MONGO = AsyncIOMotorClient(DATABASE_URL)
+DATABASE = CLIENT_MONGO[DATABASE_NAME]
+
+
+async def init_db(db: AsyncIOMotorDatabase):
+    logging.info(f"Initialise la base de données")
+    
 
 async def close_db():
     CLIENT_MONGO.close()
--- main.py
+++ main.py
@@ -30,4 +30,4 @@ app.include_router(pesees.router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run('main:app', host="192.168.88.245", port=8000, reload=True)
+    uvicorn.run('main:app', host="0.0.0.0", port=8000, reload=True)
